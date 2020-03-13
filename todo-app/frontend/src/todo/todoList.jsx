5aadc28d2fc59d3eab973548c4b9232205c07099
Jaison de Sousa <jaisonangel@hotmail.com>
2020-03-12 22:20:44 -0300

    Estudo de React

---
+++ todo-app/frontend/src/todo/todoList.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { render } from 'react-dom'
+import IconButton from '../template/iconButton'
+
+export default props => {
+
+const list = props.list || []
+  const renderRows = () => {
+      return list.map(todo => (
+          <tr key={todo._id}>
+              <td>{todo.description}</td>
+              <td>
+                  <IconButton style='danger' icon='trash-o'
+                  onClick={() = props.handleRemove(todo)} >
+
+                  </IconButton>
+              </td>
+          </tr>
+      ))
+
+  }
+
+  return (
+      <table className='table'>
+          <thead>
+              <tr>
+                  <th>Descrição</th>
+                  <th>Ações</th>
+              </tr>
+          </thead>
+          <tbody>
+              {renderRows()}
+          </tbody>
+      </table>
+
+  )
+  }
